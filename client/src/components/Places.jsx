8035e75f89b6974335c898aa3930b35c045756dd
LENOVO <maxsavchuk1105@gmail.com>
2023-02-14 18:33:45 +0200

    Add AccommodationPage`s layout

--- client/src/components/Places.jsx
+++ client/src/components/Places.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import { Link, useParams } from 'react-router-dom'
 
 const Places = () => {
+    const { action } = useParams();
+
     return (
-        <div>My places</div>
+        <div>
+            {action !== 'new' && (
+                <div className='text-center'>
+                    <Link className='bg-primary text-white rounded-full py-2 px-6 inline-flex gap-2' to={'/account/places/new'}>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                        </svg>
+                        Add new place
+                    </Link>
+                </div>
+            )}
+            {action === 'new' && (
+                <div>
+                    <form >
+                        <h2 className='text-2xl mt-4'>Title</h2>
+                        <p className='text-gray-500 text-sm'>Title for your place. should be short and catchy as in advertisement</p>
+                        <input type="text" placeholder='Title, for exampe: My lovely apartment' />
+                        <h2 className='text-2xl mt-4'>Adress</h2>
+                        <p className='text-gray-500 text-sm'>Adress to your place</p>
+                        <input type="text" placeholder='adress' />
+                        <h2 className='text-2xl mt-4'>Photos</h2>
+                        <p className='text-gray-500 text-sm'>More = better</p>
+                        <div className=' mt-2 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6'>
+                            <button className='border bg-transparent rounded-2xl p-8 text-2xl text-gray-600 hover:text-black'>+</button>
+                        </div>
+                    </form>
+                </div>
+            )
+            }
+        </div>
     )
 }
 
