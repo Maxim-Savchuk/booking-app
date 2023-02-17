334f02abd7d4ed8b464b720630a6ce5d15518e3b
LENOVO <maxsavchuk1105@gmail.com>
2023-02-17 20:54:53 +0200

    Finished photo uploading and perks

--- api/index.js
+++ api/index.js
@@ -6,6 +6,9 @@ const jwt = require('jsonwebtoken');
 const User = require('./models/User.js');
 const cookieParser = require('cookie-parser');
 const imageDownloader = require('image-downloader');
+const multer = require('multer');
+const fs = require('fs');
+
 require('dotenv').config();
 const app = express();
 
@@ -86,6 +89,22 @@ app.post('/upload-by-link', async (req, res) => {
     dest: __dirname + '/uploads/' + newName,
   });
   res.json(newName);
+});
+
+const photosMiddleware = multer({ dest: 'uploads/' });
+app.post('/upload', photosMiddleware.array('photos', 100), (req, res) => {
+  const uploadedFiles = [];
+  console.log(req.files);
+  for (let i = 0; i < req.files.length; i++) {
+    const { path, originalname } = req.files[i];
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1];
+    const newPath = path + '.' + ext;
+    fs.renameSync(path, newPath);
+    const uploadedFile = newPath.replace('uploads\\', '');
+    uploadedFiles.push(uploadedFile);
+  }
+  res.json(uploadedFiles);
 });
 
 app.listen(4000);
--- client/src/components/PlacesForm.jsx
+++ client/src/components/PlacesForm.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import axios from 'axios'
+import React from 'react';
+import axios from 'axios';
 import Perks from './Perks.jsx';
 
 const PlacesForm = () => {
@@ -57,6 +57,24 @@ const PlacesForm = () => {
         setPhotoLink('');
     }
 
+    const uploadPhoto = (e) => {
+        const files = e.target.files;
+        const data = new FormData();
+
+        for (let i = 0; i < files.length; i++) {
+            data.append('photos', files[i]);
+        }
+
+        axios.post('/upload', data, {
+            headers: { "Content-type": 'multipart/form-data' }
+        }).then(res => {
+            const { data: filenames } = res;
+            setAddedPhotos(prev => {
+                return [...prev, ...filenames]
+            });
+        });
+    }
+
     return (
         <div>
             <form >
@@ -72,17 +90,18 @@ const PlacesForm = () => {
                     </button>
                 </div>
                 <div className='mt-2 grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6'>
-                    {addedPhotos.length > 0 && addedPhotos.map(link => (
-                        <div>
-                            <img className='rounded-2xl' src={'http://localhost:4000/uploads/' + link} alt={link} />
+                    {addedPhotos.length > 0 && addedPhotos.map((link, index) => (
+                        <div className='h-32 flex' key={link + index}>
+                            <img className='rounded-2xl w-full object-cover' src={'http://localhost:4000/uploads/' + link} alt={link} />
                         </div>
                     ))}
-                    <button className='flex justify-center items-center gap-1 border bg-transparent rounded-2xl p-2 text-2xl text-gray-600 hover:text-black hover:border-black'>
+                    <label className='h-32 cursor-pointer flex justify-center items-center gap-1 border bg-transparent rounded-2xl p-2 text-2xl text-gray-600 hover:text-black hover:border-black'>
+                        <input type="file" multiple className='hidden' onChange={uploadPhoto} />
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
                         </svg>
                         Upload
-                    </button>
+                    </label>
                 </div>
                 <InputLabel text={'Description'} info={'Description of the place'} />
                 <textarea onChange={handleChange} value={description} name='description' />
